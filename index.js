e0bcaca6c819f31d9a23f366623a94a2c435dfe8
Kenn Pascascio <kenn370@gmail.com>
2018-06-13 14:43:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express(); 
+const Sequelize = require('sequelize');
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+const sequelize = new Sequelize('nodeChallenge', 'postgres', 'k12323', {
+    dialect: 'postgres'
+})
+
+sequelize.authenticate().then(() => {
+    console.log('PG connected')
+})
+
+const User = sequelize.define('user', {
+    first_name: Sequelize.STRING,
+    last_name: Sequelize.STRING,
+    age: Sequelize.INTEGER 
+})
+
+app.get('/api/user', (request, response) => {
+    User.findAll().then(users => {
+        response.status(200).json({users: users})
+    })
+})
+
+app.post('/api/user', (request, response) => {
+    console.log(request.body.firstName)
+    User.create({
+        first_name: request.body.firstName,
+        last_name: request.body.lastName,
+        age: request.body.age
+    }).then((userCreationSuccessObject) => {
+        response.status(200).json({
+            message: 'Well done',
+            userCreated: userCreationSuccessObject
+        })
+    })
+})
+
+sequelize.sync();
+
+app.listen(3000, () => {
+    console.log('The server is working!')
+})
